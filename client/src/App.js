917151123233c1be2babaa9cfec4c13b5adeef89
ddutjnrevenge <hanhnguyen210704@gmail.com>
2024-06-13 13:43:29 +0700

    modified multiple files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import OtherPage from "./OtherPage";
-import Fib from "./Fib";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
 
 function App() {
   return (
@@ -11,7 +11,6 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p></p>
           <a
             className="App-link"
             href="https://reactjs.org"
